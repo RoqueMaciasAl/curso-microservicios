d8eab714c540cad3391b2e1fe7b6e56c2455b5f5
Roque Macias Hts <maciasro@globalhitss.com>
2025-07-08 13:38:01 -0500

    Microservicio Cursos levantamiento inicial

---
+++ curso-microservicios/mcsv-cursos/src/test/java/com/curso/mcsv/cursos/McsvCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.mcsv.cursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McsvCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
